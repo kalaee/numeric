7a8a350623a0e9ec0dbd601ca5b739d63240b70f
Alex Kalaee <alex.kalaee@gmail.com>
2015-04-24 12:01:45 +0200

    Finished intepolation assignment

--- 1_interpolation/mainB.c
+++ 1_interpolation/mainB.c
@@ -1,53 +1,57 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
+#include <gsl/gsl_integration.h>
 
 #include "qspline.h"
+
+double fun(double z, void * params)
+{
+	qspline* s = (qspline*) params;
+	return qspline_eval(s,z);
+}
 
 int main(void)
 {
 	// generating source data
 	int length = 10, i;
-	double x[10], y[10];
+	double x[length], y[length];
 	FILE * f = fopen("B.dat","w");
-	for(i=0; i<10; i++)
+	for(i=0; i<length; i++)
 	{
 		x[i] = i+0.5 * sin(i);
 		y[i] = i + cos(i*i);
 		fprintf(f,"%g\t%g\n",x[i],y[i]);
 	}
 
-	// generate z-coordinates
-	int steps = 500;
-	double dz = (x[length-1]-x[0])/steps, z[steps];
-	for(i=0; i<steps; i++)
-	{
-		z[i] = x[0]+i*dz;
-	}
-
-	// finding the interpolation
+	int steps = 1000;
+	double dz = (x[length-1]-x[0])/steps, z;
+	// creating the qspline
 	qspline * s = qspline_alloc(length, x, y);
 	fprintf(f,"\n\n");
 	for(i=0; i<steps; i++)
 	{
-		fprintf(f,"%g\t%g\n",z[i],qspline_eval(s,z[i]));
+		z = x[0]+i*dz;
+		fprintf(f,"%g\t%g\t%g\t%g\n",z,qspline_eval(s,z),qspline_derivative(s,z),qspline_integral(s,z));
 	}
 
-	// finding the derivative
+	// GSL integration
+	gsl_integration_workspace* w = gsl_integration_workspace_alloc(1000);
+	double result, error;
+	gsl_function F;
+	F.function = &fun;
+	F.params = s;
 	fprintf(f,"\n\n");
-	for(i=0; i<steps; i++)
+	dz = (x[length-1]-x[0])/10;
+	for(i=0; i<10; i++)
 	{
-		fprintf(f,"%g\t%g\n",z[i],qspline_derivative(s,z[i]));
+		z = x[0]+i*dz;
+		gsl_integration_qag(&F,x[0],z,1e-7,1e-7,1000,6,w,&result,&error);
+		fprintf(f,"%g\t%g\n",z,result);
 	}
-
-	// finding the integral
-	fprintf(f,"\n\n");
-	for(i=0; i<steps; i++)
-	{
-		fprintf(f,"%g\t%g\n",z[i],qspline_integral(s,z[i]));
-	}
-
 	fclose(f);
 	qspline_free(s);
+	gsl_integration_workspace_free(w);
 
 	return 0;
 }
--- 1_interpolation/mainC.c
+++ 1_interpolation/mainC.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
+#include <gsl/gsl_integration.h>
 
 #include "cspline.h"
+
+double fun(double z, void * params)
+{
+	cspline* s = (cspline*) params;
+	return cspline_eval(s,z);
+}
 
 int main(void)
 {
@@ -19,8 +26,7 @@ int main(void)
 
 	int steps = 1000;
 	double dz = (x[length-1]-x[0])/steps, z;
-	
-	// finding creating the cspline with second derivatives 3 and -2 at the end points respectively
+	// creating the cspline with second derivatives 3 and -2 at the end points respectively
 	cspline * s = cspline_alloc(length, x, y,3,-2);
 	fprintf(f,"\n\n");
 	for(i=0; i<steps; i++)
@@ -28,9 +34,24 @@ int main(void)
 		z = x[0]+i*dz;
 		fprintf(f,"%g\t%g\t%g\t%g\n",z,cspline_eval(s,z),cspline_derivative(s,z),cspline_integral(s,z));
 	}
-	
+
+	// GSL integration
+	gsl_integration_workspace* w = gsl_integration_workspace_alloc(1000);
+	double result, error;
+	gsl_function F;
+	F.function = &fun;
+	F.params = s;
+	fprintf(f,"\n\n");
+	dz = (x[length-1]-x[0])/10;
+	for(i=0; i<10; i++)
+	{
+		z = x[0]+i*dz;
+		gsl_integration_qag(&F,x[0],z,1e-7,1e-7,1000,6,w,&result,&error);
+		fprintf(f,"%g\t%g\n",z,result);
+	}
 	fclose(f);
 	cspline_free(s);
+	gsl_integration_workspace_free(w);
 
 	return 0;
 }
