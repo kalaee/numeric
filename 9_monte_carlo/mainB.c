3aec065ef3b77d524c5d4c7dedaf8f32a959113e
a <a@kierkegaard>
2015-06-29 20:48:30 +0200

    Added a new integral to task B in 9: Monte Carlo

--- 9_monte_carlo/mainB.c
+++ 9_monte_carlo/mainB.c
@@ -4,8 +4,8 @@
 #include "adapt_2d.h"
 #include <gsl/gsl_rng.h>
 
-#define ACC	0.001
-#define EPS 0.001
+#define ACC	0.0001
+#define EPS 0.0001
 
 int counter;
 
@@ -19,6 +19,34 @@ double f1_mc(double* t)
 {
 	counter++;
 	return sin(t[0])*t[1]*exp(-t[0]*t[1]);
+}
+
+double f2_adapt(double x, double y)
+{
+	counter++;
+	double r = sqrt(x*x+y*y);
+	if(r < 1)
+	{
+		return exp(-r);
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+double f2_mc(double* t)
+{
+	counter++;
+	double r = sqrt(t[0]*t[0]+t[1]*t[1]);
+	if(r < 1)
+	{
+		return exp(-r);
+	}
+	else
+	{
+		return 0;
+	}
 }
 
 int main(void)
@@ -55,6 +83,29 @@ int main(void)
 	fprintf(stdout,"Estim. err:\t%g\n",err);
 	fprintf(stdout,"Actual err:\t%g\n",estim-exact);
 	fprintf(stdout,"Calls to function:\t%d\n\n",counter);
+
+	a[0] = -1.1; a[1] = -1.1;
+	b[0] = 1.1; b[1] = 1.1;
+	exact = 1.66028;
+	fprintf(stdout,"Integral: exp(-r) for r = sqrt(x^2+y^2) < 1, zero otherwise\n");
+	fprintf(stdout,"x from -1.1 to 1.1, y from -1.1 to 1.1\n");
+
+	fprintf(stdout,"Adaptive integrator w/ open intervals in 2D\n");
+	counter = 0;
+	estim = adapt_2d(f2_adapt,a[0],a[1],b[0],b[1],ACC,EPS,&err);
+	fprintf(stdout,"Estim:\t%g\n",estim);
+	fprintf(stdout,"Estim. err:\t%g\n",err);
+	fprintf(stdout,"Actual err:\t%g\n",estim-exact);
+	fprintf(stdout,"Calls to function:\t%d\n\n",counter);
+
+	fprintf(stdout,"Plain Monte Carlo w/ tolerance\n");
+	counter = 0;
+	estim = monte_carlo_tolerance(f2_mc,a,b,2,ACC,EPS,100,&err,r);
+	fprintf(stdout,"Estim:\t%g\n",estim);
+	fprintf(stdout,"Estim. err:\t%g\n",err);
+	fprintf(stdout,"Actual err:\t%g\n",estim-exact);
+	fprintf(stdout,"Calls to function:\t%d\n\n",counter);
+
 
 	gsl_rng_free(r);
 }
